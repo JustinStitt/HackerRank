d7be0d8222bb8d451723fda9126d4ddb0a16a71c
Justin Stitt <cpt.crixus@gmail.com>
2020-12-27 17:36:58 -0800

    🎆added divisibleSumPairs.jl (i, j loop)

---
+++ divisibleSumPairs.jl
@@ -0,0 +1,17 @@
+# link = https://www.hackerrank.com/challenges/divisible-sum-pairs
+
+function divisibleSumPairs(n, k, arr)
+    count = 0
+    for i in 1:n-1
+        for j in i+1:n
+            if (arr[i] + arr[j]) % k == 0
+                count += 1
+            end
+        end        
+    end
+    return count
+end
+
+n, k = readline() |> split |> x -> parse.(Int, x)
+arr = readline() |> split |> x -> parse.(Int, x)
+divisibleSumPairs(n, k, arr) |> println
