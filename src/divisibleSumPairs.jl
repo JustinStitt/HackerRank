0d538ce6e0e7a5de0bfeab7e6a15e24ceb5514ea
Justin Stitt <cpt.crixus@gmail.com>
2020-12-27 17:40:09 -0800

    🎪moved to src. updated divisibleSumPairs.jl to move Julian approach

--- src/divisibleSumPairs.jl
+++ src/divisibleSumPairs.jl
@@ -2,12 +2,10 @@
 
 function divisibleSumPairs(n, k, arr)
     count = 0
-    for i in 1:n-1
-        for j in i+1:n
-            if (arr[i] + arr[j]) % k == 0
-                count += 1
-            end
-        end        
+    for i in 1:n-1, j in i+1:n
+        if (arr[i] + arr[j]) % k == 0
+            count += 1
+        end
     end
     return count
 end
