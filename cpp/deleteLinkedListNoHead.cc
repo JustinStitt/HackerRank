6acf863ef91ce7939124fda10287237dd215042f
Justin Stitt <jkstitt@csu.fullerton.edu>
2021-11-14 13:31:59 -0800

    🔗 added deleteLinkedListNoHead.cc

---
+++ cpp/deleteLinkedListNoHead.cc
@@ -0,0 +1,26 @@
+// link = https://leetcode.com/problems/delete-node-in-a-linked-list
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+        node->val = node->next->val;
+        
+        if(!node->next->next) {
+            delete node->next;
+            node->next = nullptr;
+            return;
+        }
+        
+        
+        deleteNode(node->next);
+    }
+};
+
