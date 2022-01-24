494b4fd5808a8b007b5f6d2567101c37bca87541
Justin Stitt <jkstitt@csu.fullerton.edu>
2022-01-23 21:39:05 -0800

    🎯 added mergeSortedArrays.cc

---
+++ leetcode-ds/mergeSortedArrays.cc
@@ -0,0 +1,22 @@
+// link = https://leetcode.com/problems/merge-sorted-array/
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i{}, j{};
+        nums1.resize(m); // get rid of trailing zeros
+        
+        while(i < m && j < n) {
+            if(nums1[i] >= nums2[j]) {
+                nums1.insert(nums1.begin() + i, nums2[j++]);
+                ++m;
+                continue;
+            }    
+            ++i;
+        }
+        while(j < n) {
+            nums1.push_back(nums2[j++]);
+        }
+        
+    }
+};
+
