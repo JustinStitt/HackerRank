1a5f3a7f27d40f9624a9ac54309ba3b72f6180c1
Justin Stitt <jstitt007@gmail.com>
2023-06-07 01:38:20 -0700

    add 530.py

---
+++ python/530.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        best = 10**18
+        nodes = []
+
+        # bfs
+        stk = [root]
+
+        while len(stk):
+            curr = stk.pop()
+            if curr is None:
+                continue
+            stk.extend([curr.left, curr.right])
+
+            nodes.append(curr.val)
+
+        nodes = sorted(nodes)
+
+        for u, v in zip(nodes, nodes[1:]):
+            best = min(best, abs(u - v))
+            if best == 1:
+                return 1
+
+        return best
